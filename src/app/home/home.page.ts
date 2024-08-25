2839c8c2984cb265c5a6315fcb01e274caef2cbc
Alonso <amuc23@gmail.com>
2024-08-24 21:34:14 -0400

    cambios a home con slide

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper'; // Muestra más de una tarjeta parcialmente visible esto se instala con un npm install swiper
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  slideOpts = {
+    slidesPerView: 1.2, // Muestra más de una tarjeta parcialmente visible
+    spaceBetween: 10,   // Espacio entre las tarjetas
+    centeredSlides: true,
+    loop: false,         // Hace que las tarjetas se repitan en un bucle
+  };
+
+  products = [
+    {
+      name: 'Producto 1',
+      price: '$10.99',
+      description: 'Descripción del producto 1',
+      image: 'assets/img/caratula-hollow.jpeg'
+    },
+    {
+      name: 'Producto 2',
+      price: '$20.99',
+      description: 'Descripción del producto 2',
+      image: 'assets/img/caratula-hollow.jpeg'
+    },
+    {
+      name: 'Producto 3',
+      price: '$30.99',
+      description: 'Descripción del producto 3',
+      image: 'assets/img/caratula-hollow.jpeg'
+    },
+    // Opcional: Duplica las diapositivas si necesitas más para el bucle
+    {
+      name: 'Producto 4',
+      price: '$40.99',
+      description: 'Descripción del producto 4',
+      image: 'assets/img/caratula-hollow.jpeg'
+    },
+    {
+      name: 'Producto 5',
+      price: '$50.99',
+      description: 'Descripción del producto 5',
+      image: 'assets/img/caratula-hollow.jpeg'
+    }
+  ];
 
   constructor() {}
 
+  ngAfterViewInit() {
+    new Swiper('.swiper-container', this.slideOpts);
+  }
 }
