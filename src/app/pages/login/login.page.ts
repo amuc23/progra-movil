3bb054b7388834275add306ab061833e3d24bf32
K4yrYu <Vicentepalma1202@gmail.com>
2024-08-30 12:04:09 -0400

    loggin primigenio

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  usernameunlogged!:string;
+  //usernameunlogged!:string;
 
-  constructor() { }
+  //credenciales para entrar
+  Usuario_registrado:string="macaco";
+  password_registrado:string ="potato123";
+
+  //vars para escribir
+  usernameunlogged:string="";
+  password:string ="";
+
+  constructor(private router:Router, private alertController: AlertController, private toastController: ToastController) { 
+    
+  }
 
   ngOnInit() {
   }
 
+  loggin(){
+    let navigationextras: NavigationExtras = {
+      state:{
+        userconect: this.usernameunlogged
+      }
+    };
+
+    this.router.navigate(['/perfil'], navigationextras);
+  }
+
 }
+
--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  userLg: string = " "; 
+
+  constructor(private router: Router, private activedroute: ActivatedRoute) { 
+    //realizar la captura de la informacion que viene por navigationExtras
+    this.activedroute.queryParams.subscribe(param =>{
+      //validamos si viene o no información
+      if(this.router.getCurrentNavigation()?.extras.state){
+        //capturamos la informacion
+        this.userLg = this.router.getCurrentNavigation()?.extras?.state?.['userconect'];
+      }
+    });
+  }
 
   ngOnInit() {
   }
+
+  
 
 }
