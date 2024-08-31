a1803359c8a50a401e673e07308b9b4062394f4c
Alonso <amuc23@gmail.com>
2024-08-30 23:56:50 -0400

    validacion de la contra con caracteres

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -8,18 +8,24 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
   Usuario_registrado: string = "admin";
-  password_registrado: string = "123";
+  password_registrado: string = "Admin123.";
 
   usernameunlogged: string = "";
   password: string = "";
   loginError: boolean = false;
+
+  // Expresión regular para validar la contraseña
+  passwordPattern: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\.)[A-Za-z\d.]{6,}$/;
 
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
   loggin() {
-    if (this.usernameunlogged === this.Usuario_registrado && this.password === this.password_registrado) {
+    // Verifica que el usuario y la contraseña sean correctos
+    if (this.usernameunlogged === this.Usuario_registrado 
+        && this.password === this.password_registrado
+        && this.passwordPattern.test(this.password)) {
       // Redirige a perfil pasando el nombre de usuario como parámetro en la URL
       this.router.navigate(['/perfil'], {
         queryParams: { userconect: this.usernameunlogged }
