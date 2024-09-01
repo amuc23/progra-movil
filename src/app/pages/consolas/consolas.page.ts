a18474f5fb4b990755a58f05c79ae72608f1f313
K4yrYu <Vicentepalma1202@gmail.com>
2024-09-01 03:20:04 -0400

    finalizadas vistas unicas

--- src/app/pages/consolas/consolas.page.ts
+++ src/app/pages/consolas/consolas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-consolas',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsolasPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  compra(){
+    this.presentAlert('Añadido al carro','¡Gracias!');
+  }
+
+  async presentAlert(titulo:string,msj:string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: msj,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 
 }
