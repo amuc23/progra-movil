6b5832689ec7bae954ae5491d4936384e8fcddd4
K4yrYu <Vicentepalma1202@gmail.com>
2024-10-03 16:49:33 -0300

    intento de bd 2 correcciones

--- src/app/services/manejodb.service.ts
+++ src/app/services/manejodb.service.ts
@@ -26,6 +26,8 @@ export class ManejodbService {
  
    //usuario
    usuario: string = "CREATE TABLE IF NOT EXISTS usuario (id_usuario INTEGER PRIMARY KEY autoincrement, rut_usuario VARCHAR(20) NOT NULL, nombres_usuario VARCHAR(100) NOT NULL, apellidos_usuario VARCHAR(100) NOT NULL, username VARCHAR(20) NOT NULL, clave VARCHAR(12) NOT NULL, correo VARCHAR(50) NOT NULL, token_recup_clave BOOLEAN NOT NULL, estado_user BOOLEAN NOT NULL, id_rol INTEGER, FOREIGN KEY (id_rol) REFERENCES rol_usuario(id_rol));";
+
+   /*
    
    //producto
    producto: string = "CREATE TABLE IF NOT EXISTS producto (id_producto INTEGER PRIMARY KEY autoincrement, nombre_prod VARCHAR(50) NOT NULL, precio_prod INTEGER NOT NULL, stock_prod INTEGER NOT NULL, descripcion_prod TEXT NOT NULL, foto_prod BLOB, estatus BOOLEAN, id_categoria INTEGER, FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria));"; //blob para la foto
@@ -34,13 +36,13 @@ export class ManejodbService {
    venta: string = "CREATE TABLE IF NOT EXISTS venta (id_venta INTEGER PRIMARY KEY autoincrement, fecha_venta DATE NOT NULL, total INTEGER NOT NULL, id_usuario INTEGER, id_estado INTEGER , FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario), FOREIGN KEY (id_estado) REFERENCES estado(id_estado));";
  
    //detalle
-   detalle: string = "CREATE TABLE IF NOT EXISTS detalle (id_detalle INTEGER PRIMARY KEY autoincrement, cantidad_d INTEGER NOT NULL, subtotal INTEGER NOT NULL, id_venta INTEGER, id_producto INTEGER, FOREIGN KEY (id_venta) REFERENCES venta(id_venta), FOREIGN KEY (id_producto) REFERENCES producto(id_producto));";
+   detalle: string = "CREATE TABLE IF NOT EXISTS detalle (id_detalle INTEGER PRIMARY KEY autoincrement, cantidad_d INTEGER NOT NULL, subtotal INTEGER NOT NULL, estado_retiro BOOLEAN NOT NULL, id_venta INTEGER, id_producto INTEGER, FOREIGN KEY (id_venta) REFERENCES venta(id_venta), FOREIGN KEY (id_producto) REFERENCES producto(id_producto));";
  
    //resecna
    resecna: string = "CREATE TABLE IF NOT EXISTS resecna (id_resecna INTEGER PRIMARY KEY autoincrement, text_resecna TEXT NOT NULL, puntos_resecna INTEGER NOT NULL, foto_resecna BLOB, id_usuario INTEGER, id_producto, FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario), FOREIGN KEY (id_producto) REFERENCES producto(id_producto));";
  
    //favoritos(lista de deseos)
-   favoritos: string = "CREATE TABLE IF NOT EXISTS favoritos (id_favoritos INTEGER PRIMARY KEY autoincrement, fecha_creacion DATE NOT NULL, id_usuario INTEGER, id_producto, FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario), FOREIGN KEY (id_producto) REFERENCES producto(id_producto));";
+   favoritos: string = "CREATE TABLE IF NOT EXISTS favoritos (id_favoritos INTEGER PRIMARY KEY autoincrement, fecha_creacion DATE NOT NULL, id_usuario INTEGER, id_producto, FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario), FOREIGN KEY (id_producto) REFERENCES producto(id_producto));"; */
  
   
   ///////////////////////INSERTS//////////////////////////
@@ -86,7 +88,7 @@ export class ManejodbService {
       //procedemos a crear la base de datos
       this.sqlite.create({
         //necesita 2 parametros
-        name: 'megagames.db', //nombre conexion (luego del nombre se añade .db) al cambiar el nombre se creara una conexion nueva y se podra utilizar tablas nuevas
+        name: 'megagames1.db', //nombre conexion (luego del nombre se añade .db) al cambiar el nombre se creara una conexion nueva y se podra utilizar tablas nuevas
         location: 'default' //localicacion bd en default
       }).then((db: SQLiteObject)=>{ //.then() si la conexion es exitosa
         //capturar y guardar la conexion de Base de Datos
@@ -98,6 +100,7 @@ export class ManejodbService {
         this.idDBReady.next(true);
       }).catch(e=>{
         this.alertasService.presentAlert("Creacion de BD", "error creando la BD" + JSON.stringify(e));
+        console.log("ERROR CREACION BD");
       })
     })
   }
@@ -107,12 +110,18 @@ export class ManejodbService {
     try{
       //mandar a ejecutar las tablas en el orden especifico
       //se le agrega await para que el programa espere a que ejecute una linea para pasar a la siguiente
-      await this.database.executeSql(this.usuario,[])
+      await this.database.executeSql(this.rol_usuario, []);
+      await this.database.executeSql(this.estado, []);
+      await this.database.executeSql(this.categoria, []);
+      await this.database.executeSql(this.usuario, []);
 
       //generamos los insert en caso que existan
       await this.database.executeSql(this.registrousuario,[])
 
+      console.log("EXITO");
+
     }catch(e){
+      console.log("error conectando las tablas");
       this.alertasService.presentAlert("Creacion de tabla", "Error creando la tablas" + JSON.stringify(e));
     }
   }
